0641fff6dcb63451316c4037c0a342dd265948e4
Alcides <bmalcidesneto@gmail.com>
2024-11-20 11:51:18 -0300

    feat/password-container and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import styled from "styled-components";
 import GlobalStyle from "./GlobalStyles";
+import PasswordContainer from "./components/PasswordContainer";
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  background-color: #24222a;
+  border: 2px solid #3f08e6;
   width: 35%;
-  min-height: 60vh;
+  min-height: 100vh;
 `;
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <h1>Password Generator</h1>
-      <Container></Container>
+      <Container>
+        <h1>Password Generator</h1>
+        <PasswordContainer />
+      </Container>
     </>
   );
 }
