10a5e9d2283ec26d190f90bbafa84d746aa381f0
Alcides <bmalcidesneto@gmail.com>
2024-11-20 22:59:54 -0300

    feat/checkbox-container

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,15 @@
+function Checkbox() {
+  return (
+    <label htmlFor="">
+      <input type="checkbox" name="" id="" />
+      <span>Include Uppercase Letters</span>
+      <input type="checkbox" name="" id="" />
+      <span>Include Lowercase Letters</span>
+      <input type="checkbox" name="" id="" />
+      <span>Include Numbers</span>
+      <input type="checkbox" name="" id="" />
+      <span>Include Symbols</span>
+    </label>
+  );
+}
+export default Checkbox;
