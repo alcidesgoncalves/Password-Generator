dc69fa2edb48553c54204fb2a1335670c92ddf16
Alcides Neto <108681708+alcidesgoncalves@users.noreply.github.com>
2024-11-21 15:58:58 -0300

    Checkbox (#1)
    * feat/password-container and styles
    
    * feat/range-input component
    
    * feat/checkbox-container
    
    * feat/button-component
    
    * feat/range-input-refactored
    
    * feat/checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 import styled from "styled-components";
 import GlobalStyle from "./GlobalStyles";
+import PasswordContainer from "./components/PasswordContainer";
+import RangeInput from "./components/RangeInput";
+import Checkbox from "./components/Checkbox";
+import Button from "./components/Button";
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  background-color: #24222a;
+  border: 2px solid #3f08e6;
   width: 35%;
-  min-height: 60vh;
+  min-height: 80vh;
+`;
+
+const MainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  background-color: #24222a;
+  margin-top: 1em;
+  padding: 1rem;
 `;
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <h1>Password Generator</h1>
-      <Container></Container>
+      <Container>
+        <h1>Password Generator</h1>
+        <PasswordContainer />
+        <MainDiv>
+          <RangeInput />
+          <Checkbox />
+          <Button />
+        </MainDiv>
+      </Container>
     </>
   );
 }
